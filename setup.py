bfdb6eddcb71daeada86fb1873dddb474c0bacd2
Yasin Kaya <yasinkaya.121@gmail.com>
2019-01-30 15:44:39 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='sixfab_gprsiot',
-    version='1.0',
+    version='1.0.0',
     author='Yasin Kaya',
     author_email='yasinkaya.121@gmail.com',
-    description='sixfab linux libraries',
+    description='sixfab python libraries',
     license='MIT',
     url='https://github.com/sixfab/Sixfab_RPi_GPRSIoT_HAT',
     dependency_links  = [],
-	install_requires  = ['pyserial'],
+	install_requires  = ['pyserial','pigpio'],
     packages=find_packages()
 )
