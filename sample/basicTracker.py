55ae8555460dc9f746100e61a87fb65e1a735cd2
saeedjohar <saeed_johar@yahoo.com>
2019-09-13 15:18:02 +0300

    info modified and cleaned code

--- sample/basicTracker.py
+++ sample/basicTracker.py
@@ -1,46 +1,46 @@
 '''
-  basicUDP.py - This is basic UDP example.
+  basicTraker.py - This is basic Tracker example.
   Created by Yasin Kaya (selengalp), January 2, 2019.
 '''
 from gprsiot import gprsiot
-import time
+from time import sleep
 
-your_ip = "78.183.237.146" # change with your ip
-your_port = "4000" # change with your port
+your_ip = "xx.xx.xx.xx" # change with your ip
+your_port = "xxxx" # change with your port
 
 node = gprsiot.GPRSIoT()
 node.disable()
-time.sleep(2)
+sleep(2)
 
 node.enable()
-time.sleep(2)
+sleep(2)
 node.powerUp()
 
-node.sendATComm("ATE1","OK\r\n")
+#node.sendATComm("ATE1","OK\r\n")
 
 node.setIPAddress(your_ip)
-time.sleep(0.5)
+sleep(0.5)
 node.setPort(your_port)
-time.sleep(0.5)
+sleep(0.5)
 
 node.connectToOperator()
-time.sleep(0.5)
+sleep(0.5)
 node.getSignalQuality()
-time.sleep(0.5)
+sleep(0.5)
 
 node.deactivateContext()
-time.sleep(0.5)
+sleep(0.5)
 node.activateContext()
-time.sleep(0.5)
+sleep(0.5)
 
 node.closeConnection()
-time.sleep(0.5)
-time.sleep(0.5)
-node.startUDPService()
-time.sleep(0.5)
+sleep(1)
 
-node.sendDataUDP("Hello World!\r\n")
-time.sleep(0.5)
+node.startUDPService()
+sleep(0.5)
+
+node.sendDataUDP("Starting sending message!\r\n")
+sleep(0.5)
 
 counter = 0
 
@@ -54,5 +54,5 @@ while(True):
     msg = "long message"
   node.sendDataUDP(msg)
   msg = ""
-  time.sleep(1)
+  sleep(1)
     
