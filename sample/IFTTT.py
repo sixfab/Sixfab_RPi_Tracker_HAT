453be266d8768348a8513792c6a66fbe036be570
saeedjohar <saeed_johar@yahoo.com>
2019-10-03 13:25:52 +0300

    1. irrelevant lines removed 2. bugs fixed

--- sample/IFTTT.py
+++ sample/IFTTT.py
@@ -1,4 +1,4 @@
-'''
+''' 
   IFTTT.py - This is send data to IFTTT via http post method.
   Created by Metin KOC (saucompeng), September 11, 2018.
 '''
@@ -6,7 +6,7 @@ from gprsiot import gprsiot
 import time
 
 event_name = "" # change with your eventName
-api_key = ""; # change with api-key
+api_key = "" # change with api-key
 
 data = "{\"value1\":\"%d\"}"
 
@@ -14,26 +14,9 @@ node = gprsiot.GPRSIoT()
 node.disable()
 node.enable()
 node.powerUp()
+sleep(1)
 
-node.sendATComm("ATE1","OK\r\n")
-
-node.getIMEI()
-time.sleep(0.5)
-node.getFirmwareInfo()
-time.sleep(0.5)
-node.getHardwareInfo()
-time.sleep(0.5)
-
-node.setGSMBand(node.GSM_900)
-time.sleep(0.5)
-node.setMode(node.GSM_MODE)
-time.sleep(0.5)
-
-node.connectToOperator()
-time.sleep(0.5)
 node.getSignalQuality()
-time.sleep(0.5)
-node.getQueryNetworkInfo()
 time.sleep(0.5)
 
 node.deactivateContext()
