b2152efe9c75c0af64ed4ff7dbd930389b757536
saeedjohar <saeed_johar@yahoo.com>
2019-10-03 13:44:51 +0300

    url ammended

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='yasinkaya.121@gmail.com',
     description='sixfab python libraries',
     license='MIT',
-    url='https://github.com/sixfab/Sixfab_RPi_GPRSIoT_HAT',
+    url='https://github.com/sixfab/Sixfab_RPi_Tracker_HAT',
     dependency_links  = [],
 	install_requires  = ['pyserial','pigpio'],
     packages=find_packages()
